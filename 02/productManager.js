b38b5324b158cabd7dad7db4e45431825a28272c
jemperador <javier_emperador@outlook.com>
2022-12-22 14:49:18 -0300

    pruebas

--- 02/productManager.js
+++ 02/productManager.js
@@ -9,36 +9,28 @@ class ProductManager {
     return this.products;
   }
 
-  addProduct(code, title, description, price, thumbnail, stock) {
+  addProduct(title, description, price, thumbnail, code, stock) {
     this.id++;
 
     const products = {
       id: this.id,
-      code,
       title,
       description,
       price,
       thumbnail,
+      code,
       stock,
     };
 
-    //prueba1
-    /* const validIds = [products.code];
-    console.log(validIds);
-    if(!validIds.includes(this.code)) {
-        return `Ya existe un producto con este codigo: ${products.code}`
-    } */
+    const [product] = this.products.filter((product) => product.code === code);
 
-    //prueba2
-    const [product] = this.products.filter((product) => product.code === code)
-
-    if(product) {
-      return `El producto con el codigo: ${product.code} ya existe \n`
+    if (product) {
+      return `El producto con el codigo: ${product.code} ya existe \n`;
     }
 
     this.products.push(products);
 
-    return `Producto agregado con el id: ${this.id} \n`;
+    return `Se cargo el producto numero: ${products.id} satisfactoriamente  \n`;
   }
 
   getProductsById(idProduct) {
@@ -54,36 +46,28 @@ class ProductManager {
 
 const productsManager = new ProductManager();
 
+//primera consulta => []
+console.log(productsManager.getProducts());
+
 //producto1
-const codeProduct1 = "abc123";
-const titleProduct1 = "Algo";
-const descriptionProduct1 = "Lorem Ipsum";
-const priceProduct1 = 500;
-const thumbnailProduct1 = "https://picsum.photos/200";
-const stockProduct1 = 10;
+const titleProduct = "producto prueba";
+const descriptionProduct = "Lorem Ipsum";
+const priceProduct = 200;
+const thumbnailProduct = "https://picsum.photos/200";
+const codeProduct = "abc123";
+const stockProduct = 25;
 
-//producto2
-const codeProduct2 = "abc1234";
-const titleProduct2 = "Algo2";
-const descriptionProduct2 = "Lorem Ipsum2";
-const priceProduct2 = 5000;
-const thumbnailProduct2 = "https://picsum.photos/300";
-const stockProduct2 = 100;
+//carga de producto => Se cargo el producto numero: 1 satisfactoriamente
+console.log(productsManager.addProduct(titleProduct, descriptionProduct, priceProduct, thumbnailProduct, codeProduct, stockProduct));
 
-//consulta productos
+//segunda consulta => [{ id: 1, title: 'producto prueba', description: 'Lorem Ipsum', price: 200, thumbnail: 'https://picsum.photos/200', code: 'abc123', stock: 25}]
 console.log(productsManager.getProducts());
 
-//consulta agregando un producto
-console.log(productsManager.addProduct(codeProduct1, titleProduct1, descriptionProduct1, priceProduct1, thumbnailProduct1, stockProduct1));
-console.log(productsManager.addProduct(codeProduct2, titleProduct2, descriptionProduct2, priceProduct2, thumbnailProduct2, stockProduct2));
+//carga del mismo producto => El producto con el codigo: abc123 ya existe
+console.log(productsManager.addProduct(titleProduct, descriptionProduct, priceProduct, thumbnailProduct, codeProduct, stockProduct));
 
-//consulta productos
-console.log(productsManager.getProducts());
-
-//consulta agregando el mismo producto
-console.log(productsManager.addProduct(codeProduct1, titleProduct1, descriptionProduct1, priceProduct1, thumbnailProduct1, stockProduct1));
-console.log(productsManager.addProduct(codeProduct2, titleProduct2, descriptionProduct2, priceProduct2, thumbnailProduct2, stockProduct2));
-
-//consulta si existe un producto
-console.log(productsManager.getProductsById(1));
+//consulta si existe el producto => Not found
 console.log(productsManager.getProductsById(5));
+
+//consulta si existe el producto
+console.log(productsManager.getProductsById(1));
