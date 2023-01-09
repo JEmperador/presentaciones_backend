61fe7e4f9ab27e3e7e1be489d29395a0d39534ad
jemperador <javier_emperador@outlook.com>
2023-01-09 13:14:10 -0300

    se corrige autoincremental de id

--- 02/productManager.js
+++ 02/productManager.js
@@ -10,8 +10,6 @@ class ProductManager {
   }
 
   addProduct(title, description, price, thumbnail, code, stock) {
-    this.id++;
-
     const products = {
       id: this.id,
       title,
@@ -26,9 +24,10 @@ class ProductManager {
 
     if (product) {
       return `El producto con el codigo: ${product.code} ya existe \n`;
+    } else {
+      this.id++;
+      this.products.push(products);
     }
-
-    this.products.push(products);
 
     return `Se cargo el producto numero: ${products.id} satisfactoriamente  \n`;
   }
@@ -43,3 +42,41 @@ class ProductManager {
     return product;
   }
 }
+
+/* //instancia de la clase
+const productsManager = new ProductManager();
+
+//primera consulta => []
+console.log(productsManager.getProducts());
+
+//producto1
+const titleProduct = "producto prueba";
+const descriptionProduct = "Lorem Ipsum";
+const priceProduct = 200;
+const thumbnailProduct = "https://picsum.photos/200";
+const codeProduct = "abc123";
+const stockProduct = 25;
+
+//producto2
+const titleProduct2 = "producto prueba";
+const descriptionProduct2 = "Lorem Ipsum";
+const priceProduct2 = 200;
+const thumbnailProduct2 = "https://picsum.photos/200";
+const codeProduct2 = "abc1234";
+const stockProduct2 = 25;
+
+//carga de producto => Se cargo el producto numero: 1 satisfactoriamente
+console.log(productsManager.addProduct(titleProduct, descriptionProduct, priceProduct, thumbnailProduct, codeProduct, stockProduct));
+console.log(productsManager.addProduct(titleProduct2, descriptionProduct2, priceProduct2, thumbnailProduct2, codeProduct2, stockProduct2));
+
+//segunda consulta => [{ id: 1, title: 'producto prueba', description: 'Lorem Ipsum', price: 200, thumbnail: 'https://picsum.photos/200', code: 'abc123', stock: 25}]
+console.log(productsManager.getProducts());
+
+//carga del mismo producto => El producto con el codigo: abc123 ya existe
+console.log(productsManager.addProduct(titleProduct, descriptionProduct, priceProduct, thumbnailProduct, codeProduct, stockProduct));
+
+//consulta si existe el producto => Not found
+console.log(productsManager.getProductsById(5));
+
+//consulta si existe el producto
+console.log(productsManager.getProductsById(0)); */
