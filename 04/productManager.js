147e285f53f76984ecb2871bdda753064409a6bd
jemperador <javier_emperador@outlook.com>
2023-01-13 23:01:10 -0300

    correccion de log

--- 04/productManager.js
+++ 04/productManager.js
@@ -10,6 +10,7 @@ class ProductManager {
 
   async addProduct(title, description, price, thumbnail, code, stock) {
     const products = await this.getProducts();
+
     try {
       const lastId = products.reduce(
         (acc, curr) => {
@@ -33,19 +34,21 @@ class ProductManager {
       };
 
       if (products.find((product) => product.code === code)) {
-        console.log(`El producto con el codigo: ${product.code} ya existe`);
+        return console.log(`El producto con el codigo: ${product.code} ya existe`);
       } else {
         products.push(product);
         await fs.promises.writeFile(path, JSON.stringify(products, null, "\t"));
-        console.log(products);
+
+        return console.log(products);
       }
     } catch (err) {
-      console.log(err);
+      return console.log(err);
     }
   }
 
   async getProducts() {
     try {
+
       if (fs.existsSync(path)) {
         const data = await fs.promises.readFile(path, "utf-8");
         const products = JSON.parse(data);
@@ -54,7 +57,7 @@ class ProductManager {
         return [];
       }
     } catch (err) {
-      console.log(err);
+      return console.log(err);
     }
   }
 
@@ -62,6 +65,7 @@ class ProductManager {
     const products = await this.getProducts();
     try {
       const itemId = Object.values(products).find((product) => product.id === idProduct);
+
       if (itemId === undefined) {
         return console.error("El producto no existe");
       } else {
@@ -76,13 +80,15 @@ class ProductManager {
     const products = await this.getProducts();
     try {
       const index = await products.findIndex((product) => product.id === idProduct);
+
       if (index === -1) {
-        console.log("El producto no existe");
+        return console.log("El producto no existe");
       } else {
         Object.assign(products[index], propsProduct);
         await fs.promises.writeFile(path, JSON.stringify(products), "utf-8");
         const updatedProduct = products[index];
-        console.log(updatedProduct);
+
+        return console.log(updatedProduct);
       }
     } catch (err) {
       return console.error(err);
@@ -93,9 +99,11 @@ class ProductManager {
     let products = await this.getProducts();
     try {
       const product = Object.values(products).find((e) => e.id === idProduct);
+
       if (product) {
         products = products.filter((item) => item.id !== idProduct);
         await fs.promises.writeFile(path, JSON.stringify(products), "utf-8");
+
         return console.log("Producto eleminado correctamente");
       } else {
         return console.error("El producto no existe");
@@ -123,15 +131,15 @@ carga(); */
 
 /* const consultaPorId = async () => {
   console.log("----------Consulta de producto por id----------");
-  const idProduct = await manager.getProductById(2);
+  const idProduct = await manager.getProductById(1);
 };
 consultaPorId(); */
 
-/* const actualizar = async () => {
-  console.log("actualizar producto");
+const actualizar = async () => {
+  console.log("----------Actualizacion de producto----------");
   const productUpdate1 = await manager.updateProduct(1, { title: "producto prueba modificado", description: "Lorem Ipsum modificado", stock: 50 });
 };
-actualizar(); */
+actualizar();
 
 /* const borrar = async () => {
   console.log("----------Borra producto por id----------");
